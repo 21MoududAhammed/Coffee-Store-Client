2d1d1c6e457b2fb02643354978fc617bac035bbe
21MoududAhammed <21moudud@gmail.com>
2024-06-06 17:11:12 +0600

    delete operation has done

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,4 @@
-export default function ProductCard({ product }) {
-  
+export default function ProductCard({ product, onDelete }) {
   return (
     <div className="flex max-w-md overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
       <div
@@ -13,17 +12,19 @@ export default function ProductCard({ product }) {
           {product?.name}
         </h1>
         <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
-        Taste: {product?.taste}
+          Taste: {product?.taste}
         </p>
         <div className="flex mt-2 item-center">
           <p>Details: {product?.details}</p>
         </div>
         <div className="flex justify-between mt-3 item-center">
-          
           <button className="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">
             update
           </button>
-          <button className="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">
+          <button
+            className="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600"
+            onClick={() => onDelete(product?._id)}
+          >
             Delete
           </button>
         </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,6 +4,26 @@ import ProductCard from "./ProductCard";
 export default function Products() {
   const [items, setItems] = useState([]);
 
+  // delete a product 
+  const handleDelete = async (id) => {
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_BASE_URL}/coffees/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (res.status === 200) {
+        const data = await res.json();
+        alert(data?.message);
+        setItems(items.filter((item) => item._id !== id));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // get all items
   useEffect(() => {
     const fetchItems = async () => {
       try {
@@ -22,7 +42,9 @@ export default function Products() {
   return (
     <div className="grid sm:grid-cols-2 md:grid-cols-4  gap-5 m-3">
       {items?.length > 0 &&
-        items.map((item) => <ProductCard key={item._id} product={item} />)}
+        items.map((item) => (
+          <ProductCard key={item._id} product={item} onDelete={handleDelete} />
+        ))}
     </div>
   );
 }
