2b9ab829be7f7f27ef4b9f52a3e8c20db1f89169
21MoududAhammed <21moudud@gmail.com>
2024-06-07 09:52:43 +0600

    Added style to determine the active link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,27 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 export default function Nav() {
   return (
-    <nav className="flex justify-center py-5 shadow bg-green-300">
+    <nav className="flex justify-center py-5 shadow">
       <ul className="flex gap-5 font-bold">
         <li>
-          <Link to="/entry-products">Entry Product</Link>
+          <NavLink
+            to="/entry-products"
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "text-black"
+            }
+          >
+            Entry Product
+          </NavLink>
         </li>
         <li>
-          <Link to="/">Products</Link>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "text-black"
+            }
+          >
+            Products
+          </NavLink>
         </li>
       </ul>
     </nav>
